3ff009702fb26dbeda0c7b9aa2fee25ad242be1d
AnthraX1 <AnthraX1@users.noreply.github.com>
2019-03-19 01:07:07 +1100

    add optional timeout

--- elasticdump.py
+++ elasticdump.py
@@ -13,6 +13,8 @@ TIMEOUT = "1d"
 def ES21scroll(sid):
     return json.loads(requests.get("{}/_search/scroll?scroll={}".format(args.host,TIMEOUT),data=sid,verify=False).text)
 
+def display(msg):
+    sys.stderr.write(msg+"\n")
 
 def dump(es,outq,alldone):
     esversion = getVersion(es)
@@ -23,6 +25,7 @@ def dump(es,outq,alldone):
                 r = ES21scroll(r["_scroll_id"])
         else:
             r = es.search(args.index, sort=["_doc"],size=args.size, scroll=TIMEOUT,q=args.q, body=args.query)
+        display("Total docs:"+str(r["hits"]["total"]))
     else:
         fs=open(url.netloc+'_'+args.index+'.session','r')
         sid=fs.readlines()[0].strip()
@@ -32,10 +35,11 @@ def dump(es,outq,alldone):
         else:
             r = es.scroll(scroll_id=sid, scroll=TIMEOUT)
     while '_scroll_id' in r and len(r['hits']['hits'])>0:
-        sid=r['_scroll_id']
-        f=open(url.netloc+'_'+args.index+'.session','w')
-        f.write(sid+"\n")
-        f.close()
+        if sid!=r['_scroll_id']:
+            f=open(url.netloc+'_'+args.index+'.session','w')
+            f.write(sid+"\n")
+            f.close()
+            sid=r['_scroll_id']
         for row in r['hits']['hits']:
             outq.put(row)
         try:
@@ -63,12 +67,13 @@ if __name__ == "__main__":
     parser.add_argument('--host', help='ES host, http[s]://host:port',required=True)
     parser.add_argument('--index',help='Index name or index pattern, for example, logstash-* will work as well. Use _all for all indices', required=True)
     parser.add_argument('--size',help='Scroll size',default=500)
+    parser.add_argument('--timeout',help='Read timeout. Wait time for long queries.',default=300)
     group = parser.add_mutually_exclusive_group()
     group.add_argument('--query', help='Query string in Elasticsearch DSL format.')
     group.add_argument('--q', help='Query string in Lucene query format.')
     args = parser.parse_args()
     url=urlparse(args.host)
-    es=Elasticsearch(url.netloc,request_timeout=5,timeout=120)
+    es=Elasticsearch(url.netloc,request_timeout=5,timeout=args.timeout)
     if url.scheme=='https':
         es=Elasticsearch(url.netloc,use_ssl=True,verify_certs=False,request_timeout=5,timeout=120)
     outq=Queue(maxsize=50000)
